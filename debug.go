8592d0943c48fe39f35d1f179cd95006f8fba689
ah <face_monsoon.0h@icloud.com>
2023-05-14 18:11:21 -0500

    errors report line and file

--- debug.go
+++ debug.go
@@ -2,19 +2,29 @@
 // To turn on debug mode, use `go build --tags debug ...`.
 package debug
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 // Err is a non nil error passed to [Error].
-type Err struct{ error error }
+type Err struct {
+	File  string
+	Line  uint
+	error error
+}
 
-func (e *Err) Error() string { return fmt.Sprintf("debug: %s", e.error.Error()) }
 func (e *Err) Unwrap() error { return e.error }
+func (e *Err) Error() string {
+	return fmt.Sprintf("[DEBUG] { %s:%d }: %s", e.File, e.Line, e.error.Error())
+}
 
 // Error return the given error if [Debug] is true.
 // Returns nil if err is nil or [Debug] is false.
 func Error(err error) error {
 	if Debug && err != nil {
-		return &Err{error: err}
+		_, fn, ln, _ := runtime.Caller(1)
+		return &Err{File: fn, Line: uint(ln), error: err}
 	}
 	return nil
 }
